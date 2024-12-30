f11a4ff325587c8beb1613c0b51c63cf9e30e537
¨Gabs2000¨ <cesargabrielrodriguezacosta@Cesars-Mac-mini.local>
2024-12-30 04:29:43 -0600

    Day 2 almost completed

--- Beginner/D2_String_manipulation/T2_type_error.py
+++ Beginner/D2_String_manipulation/T2_type_error.py
@@ -1,1 +1,32 @@
-len(12345)
+# Functions expect a specific data type
+# Error: Not the data type expected by len(), which would be a str
+# len(12345)
+
+len("Hello")
+
+# Print the data type
+print(type("abc"))
+print(type(123))
+print(type(1.2))
+print(type(True))
+
+# Error: Invalid literal "abc", cannot convert it to number (not logical)
+# print(int("abc") + int("456"))
+
+# Use the following to conver data types
+int()
+float()
+str()
+bool()
+
+# Error: Can only concatenate str not int to str, fixed with str(len(input()))
+print("Number of letters in your name: " + str(len(input("Enter your name: "))))
+
+# Other solution or troubleshooting
+name_of_the_user = input("Enter your name: ")
+length_of_name = len(name_of_the_user)
+
+print(type("Number of letters in your name: ")) #str
+print(type(length_of_name)) #int
+
+print("Number of letters in your name: " + str(length_of_name))
---
+++ Beginner/D2_String_manipulation/T3_mathematical_operations.py
@@ -0,0 +1,18 @@
+print("My age: " + str(12))
+print(123 + 456) # Addition
+print(7 - 3) # Substraction
+print(3 * 2) # Multiplication
+print(5 / 3) # Dividing, float
+print(5 // 3) # Dividing, integer, use it carefully
+print(2**3) # Raise a number to the power of the exponent
+
+# PEMDAS Operation order
+# Parentheses, Exponents, Multiplication/Division, Addition/Substraction
+# ()
+# **
+# * OR /
+# + OR -
+
+# Exercise:
+print(3 * 3 + 3 / 3 - 3) # Returns 7.0
+print(3 * (3 + 3) / 3 - 3) # Returns 3
