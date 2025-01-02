9c5ecb800765f023e178e4721e66a51f725a02de
¨Gabs2000¨ <cesardz_2000_00@outlook.com>
2025-01-02 04:52:29 -0600

    Starting day 3

---
+++ Beginner/D3_Control_flow_logical_operators/T1_if_else.py
@@ -0,0 +1,9 @@
+# Conditional statemetns, logical operators, code blocks and scope
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry youhave to grow taller before you can ride.")
---
+++ Beginner/D3_Control_flow_logical_operators/T2_modulo_operator.py
@@ -0,0 +1,7 @@
+# Check if a number is Odd or Even
+modulo = int(input("Please write a number to check if its odd or even: "))
+
+if modulo % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
